1b3fe4f2a250234f0c2f7a1856e60d92b1a1b727
amegaeru <amegaeru@hotmail.com>
2023-04-14 14:13:10 +0900

    03.node.js\05.get_clientinformation.js

---
+++ 03.node.js/05.get_clientinformation.js
@@ -0,0 +1,11 @@
+var http = require('http');
+var server = http.createServer(function(req, res) {
+		console.log("URL: " + req.url);
+		console.log("Method: " + req.method);
+		console.log("Header[Content-Type]: " + req.headers['content-type']);
+		res.write("UrL " + req.url);
+		res.write("\nMethod: " + req.method);
+		res.write("\nHeader[Content-Type]: " + req.headers['content-type'])
+		res.end();
+}).listen(8080);
+
